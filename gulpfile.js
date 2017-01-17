e05ad60c95b8bc5f6739d85bc2ca7b11918d6ec2
Philipp <PhilSantay@gmail.com>
2017-01-17 13:15:54 +0100

    Gulp file updated - gulp-watch added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-let gulp = require("gulp");
+const gulp = require("gulp"),
+watch = require("gulp-watch");
 
 gulp.task("default", function(){
 	console.log("Default Task is running"); //!
+})
+gulp.task("change", function(){
+	console.log("Change commited!");
+})
+
+gulp.task("watch", function(){
+	watch("./app/index.html", function(){
+		gulp.start("change");
+	});
 })
