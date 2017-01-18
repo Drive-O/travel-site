325d0233898ca5b0ade23b5478c45f36e21e9a7f
Philipp <PhilSantay@gmail.com>
2017-01-18 16:52:48 +0100

    BrowserSync installed, Buttons styled

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,36 @@ postcss = require("gulp-postcss"),
 prefix = require("autoprefixer"),
 imports = require("postcss-import"),
 nesting = require("postcss-nesting"),
-vars = require("postcss-simple-vars");
+vars = require("postcss-simple-vars"),
+browserSync = require("browser-sync").create(); //just import the method create
 
 gulp.task("default", function(){
 	console.log("Default Task is running"); //!
 })
 
 gulp.task("css", function(){
-	gulp.src("./app/assets/styles/styles.css")
+	return gulp.src("./app/assets/styles/styles.css")
 	.pipe(postcss([imports, prefix, nesting, vars]))
-	.pipe(gulp.dest("./temp/styles/"));
+	.pipe(gulp.dest("./app/temp/styles/"));
 })
 
 gulp.task("watch", function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
 	watch("./app/assets/styles/**/*.css", function(){
-		gulp.start("css");
+		gulp.start("cssInject");
 	});
 })
+
+gulp.task("cssInject", ["css"], function(){ //[dependencies] -> cssInject wont start before the dependencies are running
+	return gulp.src("./app/temp/styles/styles.css")
+	.pipe(browserSync.stream());
+});
